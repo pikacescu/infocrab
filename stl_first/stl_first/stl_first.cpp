34bffab2d41f56ae99c7956c45e871b50c07190a
pikacescu <65919010+pikacescu@users.noreply.github.com>
2026-02-06 18:28:07 +0200

    vectors and classes

--- stl_first/stl_first/stl_first.cpp
+++ stl_first/stl_first/stl_first.cpp
@@ -1,6 +1,7 @@
 
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
@@ -12,26 +13,63 @@ using namespace std;
 
 class vect_work
 {
-    string obj;
+public:
 
-    vect_work(string obj) {
-        string obj = obj;
+public:
+    std::string msg;
+    vect_work() {
+        cout << "vect_work()" << endl;
+    }
+    vect_work(const std::string& str) {
+        cout << "vect_work(const string& "<< str <<")" << endl;
+        msg = str;
     }
 
-    vect_work(const vect_work  &obj1) { 
-        obj = obj1.obj;
+    vect_work(const vect_work& obj1) {
+        msg = obj1.msg;
+        cout << "vect_work(const vect_work&) " << msg<< endl;
     };
 
-
-    ~vect_work() {};
+    ~vect_work() { cout << "~vect_work() " << msg << endl; };
 };
+
+ostream& operator<< (ostream& os, vect_work& w)
+{
+    os << w.msg;
+    return os;
+}
 
 int main()
 {
+    cout << "******************************** start *************************" << endl;
+    vector <vect_work> cats;
+    cats.reserve(5);
+    cout << "size of cats: " << cats.size() << endl;
 
-                                                    //vectors//
 
-    vector<string> pets = { "cat", "tarantula", "hirondelle", "gecko" };
+    std::string x = "tuxedo";
+    for (int i = 0  ; i < 5; i++)
+    {
+        cout << "** before" << endl;
+        cats.push_back({ x + std::to_string(i)});
+
+        if (i == 4)
+        {
+            cats[i - 1].msg = "tabby";
+            cout << cats[i - 1] << endl;
+        }
+        cout << "** after" << endl;
+    }
+    cout << "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz" << endl;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << cats[i] << endl;
+    }
+    cout << "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz" << endl;
+
+    //cats.emplace_back() = "tuxedo";
+
+/*    vector<string> pets = { "cat", "tarantula", "hirondelle", "gecko" };
 
     for (string pet : pets)
         cout << pet << endl;
@@ -41,9 +79,12 @@ int main()
 
     cout << pets.at(0) << endl;   //first array element
     
-    pets[0] = "cats";              /*changes the[i]/(i) element*/    pets.at(0) = "cats";
-    cout << pets[0] << endl;                                         cout << pets.at(0) << endl;
+    pets[0] = "cats";                                                pets.at(0) = "cats";
+    cout << pets[0] << endl;                                         cout << pets.at(0) << endl;*/                                          
 
+
+
+    cout << "******************************** finish *************************" << endl;
 
 
 }
